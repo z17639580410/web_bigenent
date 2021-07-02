16c0a47ea99918a813dfae63308deb94416db095
zhangshihao <1870034951@qq.com>
2021-07-02 14:20:04 +0800

    登录界面完成

---
+++ assets/js/baseApi.js
@@ -0,0 +1,8 @@
+//注意：每次调用$.get()或$.post()或$.ajax()的时候
+//会先调用ajaxPrefilter 这个函数
+//在这个函数中可以拿到ajax提供的配置对象
+$.ajaxPrefilter(function(options){
+    //在发起请求之前统一拼接请求的根路径
+   options.url = 'http://api-breakingnews-web.itheima.net'+options.url
+   console.log(options.url)
+})
