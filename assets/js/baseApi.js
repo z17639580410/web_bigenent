706e55e239b675564caaf1802e70a32c25ef21d9
zhangshihao <1870034951@qq.com>
2021-07-03 18:42:12 +0800

    后台主页完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -11,5 +11,13 @@ if(options.url.indexOf('/my/') !== -1){
    options.headers={
     Authorization:localStorage.getItem('token')
    }
+   options.complete = function (res) {
+    if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败！'){
+        //1.强制清空token
+        localStorage.removeItem('token')
+        //2.强制跳转到登录页
+        location.href='./login.html'
+    }
+   }
 }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 $(function(){
     getUserInfo()
+    var layer = layui.layer
+    $('#btn-loginout').on('click',function(e){
+        e.preventDefault()
+        layer.confirm('确定退出？',{icon:3,title:'提示'},function(index){
+            // do something
+            //1.清空本地存储的token
+            localStorage.removeItem('token')
+            //2.跳转到登录页
+            location.href='./login.html'
+            console.log('1')
+            //关闭 confirm 询问框
+            layer.close(index)
+
+
+        })
+    
+    })
+    
 })
 function getUserInfo(){
     $.ajax({
@@ -12,7 +30,17 @@ function getUserInfo(){
         success:function(res){
           if(res.status !== 0){return layui.layer.msg('获取用户信息失败')}
           renderAvatar(res.data)
-        }
+        },
+        //无论成功还是失败最终都会调用complete函数
+        //在complete回调函数中可以使用res.responsejson拿到响应消息
+        //complete:function(res){
+         //   if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败！'){
+          //      //1.强制清空token
+           //     localStorage.removeItem('token')
+          //      //2.强制跳转到登录页
+           //     location.href='./login.html'
+          //  }
+       // }
     })
 }
 //渲染头像函数
